2687c79719424c20d2cd116d8ac716e2da732e94
Ranto <ranto.andriamanalina@novity.io>
2024-03-19 12:54:58 +0300

    render init

--- functions/middlewares/async-handler.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.asyncHandler = void 0;
-var asyncHandler = function (fn) { return function (req, res, next) {
-    return Promise.resolve(fn(req, res, next)).catch(next);
-}; };
-exports.asyncHandler = asyncHandler;
